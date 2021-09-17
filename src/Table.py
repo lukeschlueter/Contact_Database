943e52382494db1ffc9861cbe8aa4c849f2ea08d
Luke Schlueter <77989791+lukeschlueter@users.noreply.github.com>
2021-09-17 11:59:58 -0400

    Commenting

--- src/Table.py
+++ src/Table.py
@@ -1,4 +1,11 @@
 
+'''
+Created Sept 17, 2021
+
+@author Luke Schlueter
+'''
+
+
 class Table():
 
     def __init__(self):
--- src/main.py
+++ src/main.py
@@ -3,6 +3,12 @@ import mysql.connector
 from Contact import Contact
 from Database import Database
 
+'''
+Created Sept 17, 2021
+
+@author Luke Schlueter
+'''
+
 contact = Contact()
 database = Database()
 
