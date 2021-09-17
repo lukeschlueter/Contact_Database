3e13e6af05e593fd431836b17d863e44d91bf081
Luke Schlueter <77989791+lukeschlueter@users.noreply.github.com>
2021-09-17 11:41:14 -0400

    Update Contact.py

--- src/Contact.py
+++ src/Contact.py
@@ -1,4 +1,8 @@
 
+from validate_email import validate_email
+is_valid = validate_email('example@example.com')
+
+
 class Contact():
 
     def __init__(self):
@@ -6,7 +10,7 @@ class Contact():
             "first_name": input("What is your first name?\n"),
             "last_name": input("What is your last name?\n"),
             "phone_number": input("What is your phone number?\n"),
-            "email": input("What is your email?\n")
+            "email": self.checkEmail()
         }
 
     def addContact(self):
@@ -14,3 +18,10 @@ class Contact():
                 "(first_name, last_name, phone_number, email) "
                 f"VALUES ('{self.information['first_name']}', '{self.information['last_name']}', "
                 f"'{self.information['phone_number']}', '{self.information['email']}')")
+
+    def checkEmail(self):
+        is_valid = False
+        while not is_valid:
+            email = input("What is your email?\n")
+            is_valid = validate_email(email)
+        return email
